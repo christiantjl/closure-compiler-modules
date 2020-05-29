9bab50c8b50c940549cf53af72d9ee6864fa5da6
Christian Lewis <ctjl96@gmail.com>
2020-05-28 20:30:26 -0500

    BULK AUTOMATIC COMMIT

---
+++ compiled/object-assign.js
@@ -0,0 +1,14 @@
+(function (window) {
+        
+    /* inject CC output for object-assign.js */
+    var k="function"==typeof Object.defineProperties?Object.defineProperty:function(a,d,c){a!=Array.prototype&&a!=Object.prototype&&(a[d]=c.value)},l="undefined"!=typeof window&&window===this?this:"undefined"!=typeof global&&null!=global?global:this;function m(a,d){if(d){var c=l;a=a.split(".");for(var b=0;b<a.length-1;b++){var e=a[b];e in c||(c[e]={});c=c[e]}a=a[a.length-1];b=c[a];d=d(b);d!=b&&null!=d&&k(c,a,{configurable:!0,writable:!0,value:d})}}
+    m("Object.getOwnPropertySymbols",function(a){return a?a:function(){return[]}});var n="function"==typeof Object.assign?Object.assign:function(a,d){for(var c=1;c<arguments.length;c++){var b=arguments[c];if(b)for(var e in b)Object.prototype.hasOwnProperty.call(b,e)&&(a[e]=b[e])}return a};m("Object.assign",function(a){return a||n});
+    var p=Object.getOwnPropertySymbols,q=Object.prototype.hasOwnProperty,r=Object.prototype.propertyIsEnumerable,t=function(){try{if(!Object.assign)return!1;var a=new String("abc");a[5]="de";if("5"===Object.getOwnPropertyNames(a)[0])return!1;var d={};for(a=0;10>a;a++)d["_"+String.fromCharCode(a)]=a;if("0123456789"!==Object.getOwnPropertyNames(d).map(function(b){return d[b]}).join(""))return!1;var c={};"abcdefghijklmnopqrst".split("").forEach(function(b){c[b]=b});return"abcdefghijklmnopqrst"!==Object.keys(Object.assign({},
+    c)).join("")?!1:!0}catch(b){return!1}}()?Object.assign:function(a,d){if(null===a||void 0===a)throw new TypeError("Object.assign cannot be called with null or undefined");var c=Object(a);for(var b,e=1;e<arguments.length;e++){var f=Object(arguments[e]);for(var h in f)q.call(f,h)&&(c[h]=f[h]);if(p){b=p(f);for(var g=0;g<b.length;g++)r.call(f,b[g])&&(c[b[g]]=f[b[g]])}}return c};window.__NODE_MODULE__OBJECT_ASSIGN=t;
+
+    /* end CC inject */
+    var __module = (typeof module === "undefined" ? {} : module); 
+    __module.exports = window.__NODE_MODULE__OBJECT_ASSIGN;
+    return window.__NODE_MODULE__OBJECT_ASSIGN;
+
+})(this)
---
+++ test.js
@@ -0,0 +1,15 @@
+// const React = {};
+// const React = require('./node_modules/react');
+const React = require('./react_test.js');
+// const Test2 = require('./test2.js');
+
+class Test {
+    test() {
+        return React.Component;
+    }
+    test2() {
+        return Test2;
+    }
+}
+
+module.exports = Test;
